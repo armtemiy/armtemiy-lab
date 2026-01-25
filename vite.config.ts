f9cfbcb5924dcbf03218e02e86aa2ec0df8a33c7
Armtemiy <armtemiy@example.com>
2026-01-25 01:30:53 -0800

    fix: set correct base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/armtemiy-lab/', // Важно для GitHub Pages
   plugins: [react(), tailwindcss()],
 })
